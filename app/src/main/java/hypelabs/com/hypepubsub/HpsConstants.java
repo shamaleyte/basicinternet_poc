179ccf42b9c922dd20badb8398a776bf0fdc0428
shamaleyte <serhatsama@gmail.com>
2019-02-27 12:34:11 +0300

    First commit

--- app/src/main/java/hypelabs/com/hypepubsub/HpsConstants.java
+++ app/src/main/java/hypelabs/com/hypepubsub/HpsConstants.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class HpsConstants
 {
-    static public final String APP_IDENTIFIER = "{{app_identifier))";
-    static public final String ACCESS_TOKEN = "{{access_token))";
+    static public final String APP_IDENTIFIER = "e97fe507";
+    static public final String ACCESS_TOKEN = "0b7679a99ac288f1";
     static public final String HASH_ALGORITHM = "SHA-1";
     static public final int HASH_ALGORITHM_DIGEST_LENGTH = 20;
     static public final String ENCODING_STANDARD = "UTF-8";
